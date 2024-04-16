873601fadce431b63bce9532410085053acdb520
Advik <advik.b@gmail.com>
2024-04-17 00:50:43 +0530

    Create __main__.py

---
+++ PowerPage/__main__.py
@@ -0,0 +1,28 @@
+import requests
+from .errors import CacheLibNotFound
+from dataclasses import dataclass
+
+@dataclass
+class Wattpad:
+    base_url: str = "https://www.wattpad.com/api"
+    use_cache: bool = True
+
+    def __post_init__(self):
+        if self.use_cache:
+            try:
+                from diskcache import Cache
+                self.cache_obj = Cache("capacitor")
+            except (ImportError, ModuleNotFoundError) as e:
+                print("😂🫵🏻", flush=False)
+                raise CacheLibNotFound(
+                    "diskcache not found in the current python interpreter\n"
+                    "either install it using pip or set use_cache=False"
+                ) from e
+
+
+
+def main():
+    wpad = Wattpad()
+
+if __name__ == "__main__":
+    main()
