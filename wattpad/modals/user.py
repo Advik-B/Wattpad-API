75719fd6709224d908cc683878ee425fe6e000f3
Advik <advik.b@gmail.com>
2024-04-17 19:19:37 +0530

    Update user.py

--- wattpad/modals/user.py
+++ wattpad/modals/user.py
@@ -5,3 +5,11 @@ class User:
     name: str
     avatar: str
     username: str
+
+    @staticmethod
+    def from_json(json: dict):
+        return User(
+            name=json["name"],
+            avatar=json["avatar"],
+            username=json["username"]
+        )
