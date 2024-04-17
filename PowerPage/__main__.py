b679b13ad9df9261a671ede572f5184f9b5d5fee
Advik <advik.b@gmail.com>
2024-04-17 11:21:09 +0530

    Update __main__.py

--- PowerPage/__main__.py
+++ PowerPage/__main__.py
@@ -1,14 +1,27 @@
 from dataclasses import dataclass
-
 from .errors import CacheLibNotFound
+from requests import get
+from urllib.parse import urljoin
+
+DEFAULT_QUERY = {
+    "fields": "text_url,group(id,title,description,isPaywalled,url,cover,user(name,username,avatar),lastPublishedPart,"
+              "parts(id,title,text_url),tags)"
+
+}
 
 
 @dataclass
 class Wattpad:
-    base_url: str = "https://www.wattpad.com/api"
+    base_url: str = "https://www.wattpad.com"
     use_cache: bool = True
+    user_agent: str = (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 "
+        "(KHTML, like Gecko) "
+        "Chrome/123.0.0.0 Safari/537.36"
+    )
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         if self.use_cache:
             try:
                 from diskcache import Cache
@@ -20,9 +33,37 @@ class Wattpad:
                     "either install it using pip or set use_cache=False"
                 ) from e
 
+    def _fetch(self, path: str, query: dict) -> dict:
+        response = get(
+            urljoin(self.base_url, path),
+            verify=True,
+            headers={"User-Agent": self.user_agent},
+            params=query
+        )
+        print(urljoin(self.base_url, path))
+        return response.json()
+
+    def fetch(self, path: str, query: dict = None) -> dict:
+        if query is None:
+            query = DEFAULT_QUERY
+        if not self.use_cache:
+            return self._fetch(path, query)
+        response: dict  # for type checking
+        if response := self.cache_obj.get(path) is not None:
+            return response
+
+        response = self._fetch(path, query)
+        self.cache_obj[path] = response
+        return response
+
 
 def main():
-    wpad = Wattpad()
+    wpad = Wattpad(use_cache=False)
+    print(CERTFILE)
+    url = "https://www.wattpad.com"
+    print(urljoin(url, "v4/parts"))
+
+    print(wpad.fetch("v4/parts/1321853334"))
 
 
 if __name__ == "__main__":
