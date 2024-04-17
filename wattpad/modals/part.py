2af9f8594aa14094fa108cafa25474b91e7277dd
Advik <advik.b@gmail.com>
2024-04-17 19:19:27 +0530

    Update part.py

--- wattpad/modals/part.py
+++ wattpad/modals/part.py
@@ -5,3 +5,11 @@ class Part:
     id: int
     title: str
     text_url: str
+
+    @staticmethod
+    def from_json(json: dict):
+        return Part(
+            id=int(json['id']),
+            title=json['title'],
+            text_url=json['text_url']['text']
+        )
