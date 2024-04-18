f4f03ff1ac51852951fefa894af65f3842c6f39b
Advik <advik.b@gmail.com>
2024-04-18 23:40:36 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from wattpad import Wattpad, Story
+from wattpad import Wattpad, Story, RenderedPage
 
 w = Wattpad(use_cache=True)
 s = Story.from_id(362914687, w) # Owned by him
 
 last_part = s.parts[-1]
-last_part.render_with(w)
+render: RenderedPage = last_part.render_with(w)
 
 
 # print(*w.cache_obj.iterkeys(), sep='\n')
