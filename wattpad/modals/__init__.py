c31e5c5b8a924065cd8cb56c6ccf2feff0071c90
Advik <advik.b@gmail.com>
2024-04-18 23:35:44 +0530

    Update __init__.py

--- wattpad/modals/__init__.py
+++ wattpad/modals/__init__.py
@@ -3,3 +3,5 @@ from .published_part import PublishedPart
 from .tags import Tags
 from .user import User
 from .story import Story
+from .rendered_part_skeleton import RenderedPage, HTMLtags, HTML
+
