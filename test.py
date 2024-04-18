02a95dd1bbfc65c058371a364b0bb127707f3015
Advik <advik.b@gmail.com>
2024-04-18 22:43:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from wattpad import Wattpad, Story
 
 w = Wattpad(use_cache=True)
-s = Story.from_id(336166598, w)
-print(s.tags)
+s = Story.from_id(362914687, w) # Owned by him
+print(*[p.text_url for p in s.parts], sep='\n')
