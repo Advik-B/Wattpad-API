4b290033719301f22013c5dfd4717b8741019ae9
Advik <advik.b@gmail.com>
2024-04-18 20:35:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,5 @@
-import requests
+from wattpad import Wattpad, Story
 
-headers = {
-    "User-Agent":"insomnia/2023.5.8"
-}
-r = requests.get(
-    "https://www.wattpad.com/v4/parts/1321853334?fields=text_url,group(id,title,description,isPaywalled,url,cover,user(name,username,avatar),lastPublishedPart,parts(id,title,text_url),tags)",
-    headers=headers
-)
-
-print(r.content)
+w = Wattpad(use_cache=True)
+s = Story.from_id(336166598, w)
+print(s.tags)
