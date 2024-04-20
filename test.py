289db9cb3c97623904e05184a5ad409de3567219
Advik <advik.b@gmail.com>
2024-04-20 18:10:54 +0000

    Refactor test.py to render all parts of the Story

--- test.py
+++ test.py
@@ -6,13 +6,18 @@ s = Story.from_id(336166598, w)
 # s = Story.from_id(1321853334, w)
 
 
+    
+# last_part = s.parts[-1]
+# render: RenderedPage = last_part.render_with(w)
 
-last_part = s.parts[-1]
-render: RenderedPage = last_part.render_with(w)
-
-# print(*w.cache_obj.iterkeys(), sep='\n')
+# # print(*w.cache_obj.iterkeys(), sep='\n')
 
 print(s.title)
 print(s.url)
 
-render.display()
+# render.display()
+
+for part in s.parts:
+    part.render_with(w).display()
+    # print(part.text_url)
+    # print(part.id)
