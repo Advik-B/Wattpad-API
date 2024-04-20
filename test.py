2a716d594f0da3c4cf43ca3520241d86e5cdf2a3
Advik <advik.b@gmail.com>
2024-04-20 17:58:49 +0000

    Update test.py to include new Story IDs

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from wattpad import Wattpad, Story, RenderedPage
 
 w = Wattpad(use_cache=True)
-s = Story.from_id(362914687, w)  # Owned by him
+# s = Story.from_id(362914687, w)  # Owned by him
+s = Story.from_id(336166598, w)
+# s = Story.from_id(1321853334, w)
+
+
 
 last_part = s.parts[-1]
 render: RenderedPage = last_part.render_with(w)
