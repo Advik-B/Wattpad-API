663bc72819cc1ddc7e6e6781f07af57fbff9f71e
Advik <advik.b@gmail.com>
2024-04-20 18:28:40 +0000

    Refactor test.py to render the last part of the Story

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from wattpad import Wattpad, Story, RenderedPage
-from random import choice
 
 w = Wattpad(use_cache=True)
 # s = Story.from_id(362914687, w)  # Owned by him
@@ -8,12 +7,12 @@ s = Story.from_id(336166598, w)
 
 
     
-random_part = choice(s.parts)
-random_render: RenderedPage = random_part.render_with(w)
+last_part = s.parts[-1]
+render: RenderedPage = last_part.render_with(w)
 
 # print(*w.cache_obj.iterkeys(), sep='\n')
 
 print(s.title)
 print(s.url)
 
-random_render.display()
+render.display()
