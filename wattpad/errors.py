ec3c79c5eabdeed7f253e46a1cbbd46db6a656c9
Advik <advik.b@gmail.com>
2024-05-08 16:31:16 +0530

    Update errors.py

--- wattpad/errors.py
+++ wattpad/errors.py
@@ -1,7 +1,23 @@
+from urllib.parse import unquote
+
 class CacheLibNotFound(Exception):
     """
     Cache library (diskcache) not found in the current python runtime
     Install it using `pip install diskcache`
     """
 
-class NotFoundError(Exception): pass
+
+class APIerror(Exception): pass
+
+class NotJsonError(Exception):
+    def __init__(self, data: str):
+        super.__init__("Could not decode the response as JSON")
+        self.add_note("Response: " + data)
+
+
+class NotFoundError(APIerror):
+    def __init__(self, url: str):
+        self.url = url
+        super().__init__(f"[404] The requested URL was not found")
+        self.add_note(f"Requested URL: {unquote(url)}")
+        self.add_note("The requested URL has been unquoted for better readability")
