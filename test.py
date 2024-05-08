ce3fc78868509df9e7a545cd2c3d50751eaa1395
Advik <advik.b@gmail.com>
2024-05-08 16:30:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,13 @@
 from wattpad import Wattpad, Story, RenderedPage
 
 w = Wattpad(use_cache=True)
-# s = Story.from_id(362914687, w)  # Owned by him
-s = Story.from_id(336166598, w)
-another_s = Story.from_partid(1321853334, w)
-
-
-    
-# print(*w.cache_obj.iterkeys(), sep='\n')
-
+s = Story.from_partid(362914687, w)  # Owned by him
+# s = Story.from_id(336166598, w)
 print(s.title)
-print(s.author)
-print(s.description)
+def test_takendown_story():
+    another_s = Story.from_partid(1321853334, w)
+    print(another_s.title)
+    print(another_s.author)
+    print(another_s.description)
+    print(another_s.tags)
 
-print('='*80)
-
-print(another_s.title)
-print(another_s.author)
-print(another_s.description)
